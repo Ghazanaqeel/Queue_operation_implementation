90e6fa5a61b559fb328ff75615f3243794f5dc5f
Ghazanaqeel <46938128+Ghazanaqeel@users.noreply.github.com>
2019-01-26 02:51:23 +0500

    Add files via upload

---
+++ Queue_operations/Queue_operations/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Queue myqueue=new Queue();
+            myqueue.enque(6);
+            myqueue.enque(4);
+             myqueue.enque(8);
+             myqueue.enque(9);
+             myqueue.enque(40);
+            
+
+             Console.WriteLine("pop element are ");
+             myqueue.dequeu();
+             myqueue.dequeu();
+             Console.WriteLine("peek element is ");
+             myqueue.peak();
+             Console.WriteLine("count are");
+             myqueue.count();
+            
+        }
+    }
+    public class Queue
+    {   int rear=0;
+        int front=0;
+        
+      
+        int[]Queue1=new int[5];
+
+
+        public void enque(int e)
+        {
+            if (rear == Queue1.Length)
+            {
+
+             Console.WriteLine("full");
+           }
+        else
+            {
+                Queue1[rear++] = e;
+                Console.WriteLine(e);
+            }
+        }
+        public void dequeu()
+        {
+            if (rear == front)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                {
+                    int x;
+                    x = Queue1[front];
+                    front = front + 1;
+                    Console.WriteLine(x);
+                }
+
+            }
+        }
+            
+            public void peak()
+        {
+         int y;
+            y=Queue1[front];
+            Console.WriteLine(y);
+        }
+
+
+            public void count()
+            {
+                Console.WriteLine(front+1);
+            }
+    }
+    
+    }
+
